aee885d63163b5c08ee78a83750531f3fdc67f77
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-03-30 22:31:32 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-// Packages / Dependencies.
 const express = require('express');
 const path = require('path');
-// PORT
-const PORT = process.env.PORT || 3001;
-// Express function.
+
+const PORT = process.env.port || 3001;
+
 const app = express();
 
-// Recognizes the incoming Request Object as a JSON Object.
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-// It parses incoming requests with urlencoded payloads and is based on body-parser.
 app.use(express.urlencoded({ extended: true }));
-// Makes the public folder available to the client.
+app.use('/api', api);
+
 app.use(express.static('public'));
 
-// Routes.
 const notes = require('./routes/notes');
-// Loads the router module in the app.
 app.use('/api/notes', notes);
 
-// GET Route for notes page.
-app.get('/notes', (req, res) => {
-    // Logs the request to the terminal.
-    console.info(`${req.method} request received for ${req.path}`);
-    res.sendFile(path.join(__dirname, '/public/pages/notes.html'));
-})
-
-// GET Route for homepage / Fallback route.
+// GET Route for homepage
 app.get('*', (req, res) => {
-    // Logs the request to the terminal.
-    console.info(`${req.method} request received for ${req.path}`);
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+  console.info(`${req.method} request received for ${req.path}`);
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
-// Listens the PORT and starts node.
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
+// GET Route for notes page
+app.get('/notes', (req, res) => {
+  console.info(`${req.method} request received for ${req.path}`);
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
