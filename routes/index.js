d9e2ad9cde311e291e3cbdccb1a442c9e64c0aea
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-03-30 22:12:06 -0400

    fixed index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const router = require('express').Router()
+const path = require('path')
+const apiRoutes = require('./api')
+router.use('/api', apiRoutes)
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
 
-// Import our modular routers for /notes
-const notesRouter = require('./notes');
-
-const app = express();
-
-app.use('/notes', notesRouter);
-
-module.exports = app;
+module.exports = router;
